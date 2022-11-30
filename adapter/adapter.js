c6dfd9a229087b9a39f36b22c1c6f40e8d7e96fb
Eduardo Lima <eduardolima.tecnico@gmail.com>
2022-11-29 23:29:15 -0300

    feat: montando nodes

--- adapter/adapter.js
+++ adapter/adapter.js
@@ -37,47 +37,13 @@ fs.readFile("export/bpTemporary.json", (err, data) => {
 
     if(objectData["bpmn:definitions"]["bpmn:process"][0]["bpmn:dataObjectReference"] !== (null || undefined)){
         dataBlueprint.name = objectData["bpmn:definitions"]["bpmn:process"][0]["bpmn:dataObjectReference"][0]['$']["name"],
-        dataBlueprint.description = objectData["bpmn:definitions"]["bpmn:process"][0]["bpmn:dataObjectReference"][0]['$']["name"].replace(/-/g, ' ')
+        dataBlueprint.description = objectData["bpmn:definitions"]["bpmn:process"][0]["bpmn:dataObjectReference"][0]['$']["name"][0].toUpperCase() + objectData["bpmn:definitions"]["bpmn:process"][0]["bpmn:dataObjectReference"][0]['$']["name"].toUpperCase().slice(1).toLowerCase().replace(/-/g, ' ')
     }else{
         dataBlueprint.name = "_____name_____",
         dataBlueprint.description = "_____description_____"
     }
 
-    // Start Ok
-
-    if(objectData["bpmn:definitions"]["bpmn:process"][0]['bpmn:startEvent'] !== (null || undefined)){
-        
-        for(let count=0; count < objectData["bpmn:definitions"]["bpmn:process"][0]["bpmn:laneSet"][0]['bpmn:lane'].length; count++){
-            const valueLane = objectData["bpmn:definitions"]["bpmn:process"][0]['bpmn:startEvent'][0]['$']['id'] 
-            const returnValue = objectData["bpmn:definitions"]["bpmn:process"][0]["bpmn:laneSet"][0]['bpmn:lane'][count]['bpmn:flowNodeRef'].filter(value => (value === valueLane));
-            const laneName = objectData["bpmn:definitions"]["bpmn:process"][0]["bpmn:laneSet"][0]['bpmn:lane'][count]['$']['name'];
-            if(returnValue[0] === valueLane){
-                dataBlueprint.blueprint_spec.nodes.push(
-                    {
-                        id: objectData["bpmn:definitions"]["bpmn:process"][0]['bpmn:startEvent'][0]['$']['name'],
-                        type: "Start",
-                        name: objectData["bpmn:definitions"]["bpmn:process"][0]['bpmn:startEvent'][0]['$']['name'].replace(/-/g, ' '),
-                        next: "_____________",
-                        parameters: {
-                            input_schema: {
-                                type: "object",
-                                properties: {
-                                    name: { type: "string" },
-                                },
-                            },
-                        },
-                        lane_id: laneName,
-                    },
-                )
-            }
-        }        
-    }else{
-        console.log('startNode nonexistent!');
-    }
-
     //functions
-
-        //const aaa = objectData["bpmn:definitions"]["bpmn:process"][0]['bpmn:startEvent'][0]["bpmn:outgoing"][0]
 
     const processObject = objectData["bpmn:definitions"]["bpmn:process"][0]
     const keysProcess = Object.keys(processObject)
@@ -85,26 +51,53 @@ fs.readFile("export/bpTemporary.json", (err, data) => {
     keysProcess.splice(0, 0, keysProcess.splice(3, 1)[0]);
     console.log(keysProcess)
     
+    let typeValue = "_____"
+    let laneValue = "_____"
     
     blockExtern:{
     for(let count=0; count<keysProcess.length; count++){
         var v1 = (objectData["bpmn:definitions"]["bpmn:process"][0][keysProcess[count]][0]['bpmn:outgoing'])
-        //console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> OUT", v1, keysProcess[count])
             for(let i=0; i<keysProcess.length; i++) {
                 for(let j=0; j<objectData["bpmn:definitions"]["bpmn:process"][0][keysProcess[i]].length; j++){
-                        //console.log("ENTRADA: ", objectData["bpmn:definitions"]["bpmn:process"][0][keysProcess[i]][j]['bpmn:incoming'], " - SAÍDA:", objectData["bpmn:definitions"]["bpmn:process"][0][keysProcess[i]][j]['bpmn:outgoing'])
-                        if(objectData["bpmn:definitions"]["bpmn:process"][0][keysProcess[i]][0]['$']['name'] !== (null || undefined)){
-                            dataBlueprint.blueprint_spec.nodes.push(
-                                //{opah: `${i} => ${objectData["bpmn:definitions"]["bpmn:process"][0][keysProcess[i]][0]['$']['name']}`}
-                                {opah: objectData["bpmn:definitions"]["bpmn:process"][0][keysProcess[i]][j]['$']['name']}
-                            )
-                        }  
+                    if(objectData["bpmn:definitions"]["bpmn:process"][0][keysProcess[i]][0]['$']['name'] !== (null || undefined)){
+                        
+                        if ([keysProcess[i]][j] === "bpmn:startEvent") {
+                            typeValue = "Start"
+                        } else if ([keysProcess[i]][j] === "bpmn:endEvent") {
+                            typeValue = "Finish";
+                        } else if ([keysProcess[i]][j] === "bpmn:exclusiveGateway") {
+                            typeValue = "Flow";
+                        } else if ([keysProcess[i]][j] === "bpmn:userTask") {
+                            typeValue = "UserTask";
+                        } else if ([keysProcess[i]][j] === "bpmn:serviceTask") {
+                            typeValue = "SystemTask";
+                        }
+
+                        // Lanes
+
+                        for (let h = 0; h < objectData["bpmn:definitions"]["bpmn:process"][0]["bpmn:laneSet"][0]['bpmn:lane'].length; h++) {
+                            let valor1 = objectData["bpmn:definitions"]["bpmn:process"][0]["bpmn:laneSet"][0]['bpmn:lane'][h]['bpmn:flowNodeRef'] 
+                            let valor2 = objectData["bpmn:definitions"]["bpmn:process"][0][keysProcess[i]][j]['$']['id']; 
+
+                            if (valor1.includes(valor2)) {
+                                laneValue = objectData["bpmn:definitions"]["bpmn:process"][0]["bpmn:laneSet"][0]['bpmn:lane'][h]['$']['name']
+
+                                dataBlueprint.blueprint_spec.nodes.push({
+                                    id: objectData["bpmn:definitions"]["bpmn:process"][0][keysProcess[i]][j]['$']['name'],
+                                    type: typeValue,
+                                    name: objectData["bpmn:definitions"]["bpmn:process"][0][keysProcess[i]][j]['$']['name'][0].toUpperCase() + objectData["bpmn:definitions"]["bpmn:process"][0][keysProcess[i]][j]['$']['name'].toUpperCase().slice(1).toLowerCase().replace(/-/g, ' '),
+                                    next: "_____________",
+                                    parameters: {},
+                                    lane_id: laneValue,
+                                })
+                            } 
+                        }
+                    }  
                 }
             }
         break blockExtern;      
         }
-    }    
-      
+    }      
 
     // Export and Test Ok
 
